bc607c615f2beaf2687eb7409ec3c851361459ae
Yolotl <A01652823@itesm.mx>
2019-03-07 14:26:41 -0600

    Initial Commit

---
+++ GymTec/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GymTec
+//
+//  Created by Yair Pimentel on 3/7/19.
+//  Copyright © 2019 Yair Pimentel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GymTec/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GymTec
+//
+//  Created by Yair Pimentel on 3/7/19.
+//  Copyright © 2019 Yair Pimentel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
