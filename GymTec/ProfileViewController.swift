55a4d7e1575202617e631c6379c2e14bb35e7f95
KevinRuvalcaba <cdt307@iMac1-359.local>
2019-03-16 14:09:39 -0600

    Navigation termida
    Mucha navegación, mucha hambre

---
+++ GymTec/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  GymTec
+//
+//  Created by cdt307 on 3/16/19.
+//  Copyright © 2019 Yair Pimentel. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func LogOut(_ sender: UIButton) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
