7107ea927069bff69b5f8fd80c08def8b2ad61fb
KevinRuvalcaba <cdt307@iMac1-359.local>
2019-04-05 15:33:33 -0600

    Arreglado

--- GymTec/Gimnasio360ViewController.swift
+++ GymTec/Gimnasio360ViewController.swift
@@ -29,7 +29,7 @@ class Gimnasio360ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.autoenablesDefaultLighting = true
         
         let scene = SCNScene()
-        let esfera = SCNSphere(radius: 3.0)
+        let esfera = SCNSphere(radius: 4.0)
         let materialEsfera = SCNMaterial()
         
         materialEsfera.diffuse.contents = UIImage(named:"305HPanono - Conversion de energia.jpg")
@@ -37,18 +37,63 @@ class Gimnasio360ViewController: UIViewController, ARSCNViewDelegate {
         
         GYM.geometry = esfera
         GYM.geometry?.materials = [materialEsfera]
-        GYM.geometry?.firstMaterial?.specular.contents = UIColor.white
         GYM.position = SCNVector3(x:0, y:0, z:0)
         scene.rootNode.addChildNode(GYM)
-        
-        let pinchGestureRecognizer = UIPinchGestureRecognizer (target: self, action: #selector(escalado))
-        
-        sceneView.addGestureRecognizer(pinchGestureRecognizer)
         
         sceneView.scene = scene
         
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
+     
+     return node
+     }
+     */
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
     }
     
 
